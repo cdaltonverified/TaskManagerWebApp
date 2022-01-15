43a19c0d682d8e4d7eb20dd44f2406939e83c376
Dalton <daltonowoh@gmail.com>
2022-01-15 19:01:48 +0100

    defined base app & ckeditor

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'baseapp.apps.BaseappConfig',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
