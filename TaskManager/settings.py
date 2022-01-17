e4696cbdbfdf3c2317925a766ca121a15cbcef28
Dalton <daltonowoh@gmail.com>
2022-01-17 17:33:48 +0100

    debug

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebeg2o&h%o-0lm&q4c7-lgze==aahc5q54vaiz2m-1pq$&-(kb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'task-manager-system.herokuapp.com']
 
