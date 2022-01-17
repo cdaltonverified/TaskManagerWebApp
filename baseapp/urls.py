856c506a59dce2ae5eb14195ae2b307fcf419226
Dalton <daltonowoh@gmail.com>
2022-01-17 16:44:40 +0100

    cleanup requirements

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from django.conf.urls import url
-from django.conf import settings
-from django.views.static import serve
 from .views import ListAllTasks, CreateNewTask, UpdateTask, DeleteTask
 
 urlpatterns = [
@@ -9,6 +6,4 @@ urlpatterns = [
     path('task-new/', CreateNewTask.as_view(), name='task-new'),
     path('task-edit/<int:pk>/', UpdateTask.as_view(), name='task-edit'),
     path('task-delete/<int:pk>/', DeleteTask.as_view(), name='task-delete'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
